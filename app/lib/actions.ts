187f68c14a9d8cbf6cdf7f094fc3ffbbf6efff34
Aicirou <ajaykumarmeena808@gmail.com>
2024-02-15 20:42:10 +0530

    Chapter 14 Improving Accessibility(server-side form validation, useFormState hook) ended!

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,25 +7,52 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(), //input elements with type="number" actually return a string, not a number!
-  status: z.enum(['paid', 'pending']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce.number().gt(0, {
+    message: 'Please enter an amount greater than $0.00.',
+  }), //input elements with type="number" actually return a string, not a number!
+  status: z.enum(['paid', 'pending'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  //Extract the data from formData
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
-  const { customerId, amount, status } = CreateInvoice.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Validate form fields using Zod
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+  // console.log(`validatedFields:`, JSON.stringify(validatedFields, null, 2)); //test it out
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100; //convert to cents
   const date = new Date().toISOString().split('T')[0]; //get today's date
-  //send the data to the server
+
+  // Insert data into the database
   try {
     await sql`
   INSERT INTO invoices (customer_id, amount, status, date)
@@ -46,12 +73,25 @@ export async function createInvoice(formData: FormData) {
 //Use Zod to update the expected types
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(id: string, formData: FormData) {
-  const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
+  const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Update Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
   try {
